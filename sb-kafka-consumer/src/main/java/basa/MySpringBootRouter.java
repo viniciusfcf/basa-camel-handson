59875116c8a2943e5002324101b656dacf5bb095
Vinicius Ferraz Campos Florentino <vinicius.ferraz@gmail.com>
2024-03-10 22:44:09 -0300

    consumer

---
+++ sb-kafka-consumer/src/main/java/basa/MySpringBootRouter.java
@@ -0,0 +1,25 @@
+package basa;
+
+import org.apache.camel.Exchange;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.model.rest.RestBindingMode;
+import org.springframework.stereotype.Component;
+
+/**
+ * A simple Camel route that triggers from a timer and calls a bean and prints to system out.
+ * <p/>
+ * Use <tt>@Component</tt> to make Camel auto detect this route when starting.
+ */
+@Component
+public class MySpringBootRouter extends RouteBuilder {
+
+    @Override
+    public void configure() {
+
+        from("kafka:ordens")
+            .to("log:kafka")
+        ;
+
+    }
+
+}
